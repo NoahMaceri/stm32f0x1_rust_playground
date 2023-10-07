680738ad15439caa3f827bda2d26e0ffe5378cd0
Noah Maceri <noah.maceri@gmail.com>
2023-10-07 13:48:31 -0400

    LEDs from button irq

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,40 @@
 #![no_std]
 #![allow(unused_imports)]
 
+use hal::pac::syscfg;
 use panic_halt as _;
 
-use stm32f0xx_hal::{
+use stm32f0xx_hal as hal;
+
+use crate::hal::{
     delay::Delay,
-    pac,
+    gpio::*,
+    pac::{interrupt, Interrupt, Peripherals, EXTI},
     prelude::*,
 };
 
-use cortex_m::peripheral::Peripherals;
-use cortex_m_semihosting::debug;
+use cortex_m::{interrupt::Mutex, peripheral::Peripherals as c_m_Peripherals};
 use cortex_m_rt::entry;
 
-// For printing messages in the console using semihosting
+use core::{cell::RefCell, ops::DerefMut};
+
+// For printing to the host
 use core::fmt::Write;
 use cortex_m_semihosting::hio;
 
-// Switch HAL
-use switch_hal::{ActiveHigh, OutputSwitch, Switch, IntoSwitch, ToggleableOutputSwitch};
+// Swtich HAL
+use switch_hal::{ActiveHigh, InputSwitch, IntoSwitch, OutputSwitch, Switch};
+
+// Make our LED globally available
+static BLUE_LED: Mutex<RefCell<Option<gpioc::PC8<Output<PushPull>>>>> = Mutex::new(RefCell::new(None));
+static GREEN_LED: Mutex<RefCell<Option<gpioc::PC9<Output<PushPull>>>>> = Mutex::new(RefCell::new(None));
+
+// Make our delay provider globally available
+static DELAY: Mutex<RefCell<Option<Delay>>> = Mutex::new(RefCell::new(None));
+
+// Make external interrupt registers globally available
+static INT: Mutex<RefCell<Option<EXTI>>> = Mutex::new(RefCell::new(None));
+
 
 // Quick println implementation
 macro_rules! println {
@@ -30,35 +46,85 @@ macro_rules! println {
 
 #[entry]
 fn main() -> ! {
-    // Get peripherals
-    let mut perifs = pac::Peripherals::take().unwrap();
-    let core_perifs = Peripherals::take().unwrap();
+    if let (Some(p), Some(cp)) = (Peripherals::take(), c_m_Peripherals::take()) {
+        cortex_m::interrupt::free(move |cs| {
+            // Enable clock for SYSCFG
+            let rcc = p.RCC;
+            rcc.apb2enr.modify(|_, w| w.syscfgen().set_bit());
 
-    // Setup clocks
-    let mut rcc = perifs.RCC.configure().sysclk(8.mhz()).freeze(&mut perifs.FLASH);
-    let mut delay = Delay::new(core_perifs.SYST, &rcc);
+            let mut flash = p.FLASH;
+            let mut rcc = rcc.configure().sysclk(8.mhz()).freeze(&mut flash);
 
-    // Setup GPIO
-    let gpioc = perifs.GPIOC.split(&mut rcc);
+            let gpioa = p.GPIOA.split(&mut rcc);
+            let gpioc = p.GPIOC.split(&mut rcc);
+            let syscfg = p.SYSCFG;
+            let exti = p.EXTI;
 
-    // Setup blue and green LEDs
-    let (mut blue_led, mut green_led) = cortex_m::interrupt::free(|cs| {
-        (
-            gpioc.pc8.into_push_pull_output(cs).into_active_high_switch(),
-            gpioc.pc9.into_push_pull_output(cs).into_active_high_switch(),
-        )
-    });
+            let _ = gpioa.pa0.into_pull_down_input(cs);
 
-    // Print message
-    println!("LEDs setup!");
-    blue_led.on().unwrap();
-    green_led.off().unwrap();
+            let mut blue_led = gpioc.pc8.into_push_pull_output(cs);
+            let mut green_led = gpioc.pc9.into_push_pull_output(cs);
 
-    // Loop
+            // Turn off LED
+            blue_led.set_low().unwrap();
+            green_led.set_low().unwrap();
+
+            // Initialise delay provider
+            let delay = Delay::new(cp.SYST, &rcc);
+
+            // Enable external interrupt
+            syscfg.exticr1.modify(|_, w| unsafe { w.exti0().bits(0) });
+
+            // Set interrupt request mask
+            exti.imr.modify(|_, w| w.mr0().set_bit());
+
+            // Set interrupt falling trigger
+            exti.ftsr.modify(|_, w| w.tr0().set_bit());
+
+            // Move control over LED and DELAY and EXTI into global mutexes
+            *BLUE_LED.borrow(cs).borrow_mut() = Some(blue_led);
+            *GREEN_LED.borrow(cs).borrow_mut() = Some(green_led);
+            *DELAY.borrow(cs).borrow_mut() = Some(delay);
+            *INT.borrow(cs).borrow_mut() = Some(exti);
+
+            // Enable EXTI IRQ, set prio 1 and clear any pending IRQs
+            let mut nvic = cp.NVIC;
+            unsafe {
+                nvic.set_priority(Interrupt::EXTI0_1, 1);
+                cortex_m::peripheral::NVIC::unmask(Interrupt::EXTI0_1);
+            }
+            cortex_m::peripheral::NVIC::unpend(Interrupt::EXTI0_1);
+        });
+    }
+
     loop {
-        blue_led.toggle().unwrap();
-        green_led.toggle().unwrap();
-        delay.delay_ms(500_u16);
-    };
+        continue;
+    }
+}
 
+// Define an interupt handler, i.e. function to call when interrupt occurs. Here if our external
+// interrupt trips when the button is pressed and will light the LED for a second
+#[interrupt]
+fn EXTI0_1() {
+    // Enter critical section
+    cortex_m::interrupt::free(|cs| {
+        // Obtain all Mutex protected resources
+        if let (&mut Some(ref mut blue_led), &mut Some(ref mut green_led), &mut Some(ref mut delay), &mut Some(ref mut exti)) = (
+            BLUE_LED.borrow(cs).borrow_mut().deref_mut(),
+            GREEN_LED.borrow(cs).borrow_mut().deref_mut(),
+            DELAY.borrow(cs).borrow_mut().deref_mut(),
+            INT.borrow(cs).borrow_mut().deref_mut(),
+        ) {
+            println!("Button pressed");
+            // Turn on LED
+            blue_led.toggle().ok();
+            green_led.toggle().ok();
+
+            // Delay for a second
+            delay.delay_ms(250_u16);
+
+            // Clear event triggering the interrupt
+            exti.pr.write(|w| w.pr0().set_bit());
+        }
+    });
 }
